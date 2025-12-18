ba61ebb84fa60729ecd17886bd97fe131230e2c5
Sheriff U3 <210896603+Sheriff-Unit-3@users.noreply.github.com>
2025-12-18 17:46:54 -0600

    Added Nightstand code for future usage

--- mods/BLK_CORE/storage/storage.lua
+++ mods/BLK_CORE/storage/storage.lua
@@ -35,3 +35,39 @@ core.register_craft({
         {"group:wood_planks", "group:wood_planks", "group:wood_planks"}
     }
 })
+
+-- Night stand
+--[[
+core.register_node("blk_storage:nightstand", {
+    drawtype = "nodebox",
+    tiles = {
+    
+    },
+    node_box = {
+    	type = "fixed",
+	    fixed = {
+	    	{-0.5000, 0.5000, -0.5000, 0.5000, 1.000, 0.5000},
+	    	{-0.5000, -0.5000, -0.5000, -0.3750, 0.5000, -0.3750},
+	    	{0.3750, -0.5000, -0.5000, 0.5000, 0.5000, -0.3750},
+	    	{-0.5000, -0.5000, 0.3750, -0.3750, 0.5000, 0.5000},
+	    	{0.3750, -0.5000, 0.3750, 0.5000, 0.5000, 0.5000}
+	    }
+    }
+    sounds = {
+    
+    },
+    groups = {choppy = 1, storage = 1}
+    on_construct = function(pos)
+        local meta = core.get_meta(pos)
+        local inv = meta:get_inventory()
+        inv:set_size("main", 36)
+        meta:set_string("formspec",
+            "formspec_version[6]"..
+            "size[11.5,11.5]"..
+            "list[current_player;main;0.3,6.5;9,4;0]"..
+            "list[context;main;0.3,0.9;9,3;0]"..
+            "label[4.85,0.6;Nightstand]"..
+            "label[4.9,6.1;Inventory]"
+        )
+    end
+]]
