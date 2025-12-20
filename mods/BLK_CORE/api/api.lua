7d9c663f68f5ffa8347c670b0a12311b5df4289e
Sheriff U3 <210896603+Sheriff-Unit-3@users.noreply.github.com>
2025-12-20 15:53:23 -0600

    Added some new sounds

--- mods/BLK_CORE/api/api.lua
+++ mods/BLK_CORE/api/api.lua
@@ -1,7 +1,18 @@
+
+-- Sound Functions
 blk.wood_sounds = function()
     return {
         footstep = {name = "blk_woodwalk"},
-        dig = {name = "blk_wooddig"}
+        dig = {name = "blk_wooddig"},
+        dug = {name = "blk_wooddug"},
+        place_failed = {name = "blk_failed"}
+    }
+end
+
+blk.dirt_sounds = function()
+    return {
+        footstep = {name = "blk_dirtwalk"},
+        place_failed = {name = "blk_failed"}
     }
 end
 
@@ -9,21 +20,25 @@ blk.stone_sounds = function(type)
     if type == "stone" then
         return {
             footstep = {name = "blk_stonewalk"},
-            dig = {name = "blk_stonedig"}
+            dig = {name = "blk_stonedig"},
+            place_failed = {name = "blk_failed"}
         }
     elseif type == "gravel" then
         return {
-            footstep = {name = "blk_gravelwalk"}
+            footstep = {name = "blk_gravelwalk"},
+            place_failed = {name = "blk_failed"}
         }
     elseif type == "metal" then
         return {
             footstep = {name = "blk_stonewalk"},
             dig = {name = "blk_metaldig"},
-            dug = {name = "blk_metaldug"}
+            dug = {name = "blk_metaldug"},
+            place_failed = {name = "blk_failed"}
         }
     elseif type == "gem" then
         return {
-            footstep = {name = "blk_stonewalk"}
+            footstep = {name = "blk_stonewalk"},
+            place_failed = {name = "blk_failed"}
         }
     end
 end
@@ -32,21 +47,25 @@ blk.metal_sounds = function()
     return {
         footstep = {name = "blk_metalwalk"},
         dig = {name = "blk_metaldig"},
-        dug = {name = "blk_metaldug"}
+        dug = {name = "blk_metaldug"},
+        place_failed = {name = "blk_failed"}
     }
 end
 
 
 blk.snow_sounds = function()
     return {
-        footstep = {name = "blk_snowwalk"}
+        footstep = {name = "blk_snowwalk"},
+        dig = {name = "blk_snowdig"},
+        place_failed = {name = "blk_failed"}
     }
 end
 
 blk.glass_sounds = function()
     return {
         dig = {name = "blk_glasshit"},
-        dug = {name = "blk_glassdug"}
+        dug = {name = "blk_glassdug"},
+        place_failed = {name = "blk_failed"}
     }
 end
 
@@ -54,6 +73,7 @@ blk.leaves_sounds = function()
     return {
         footstep = {name = "blk_leaveswalk"},
         dig = {name = "blk_leavesdig"},
-        dug = {name = "blk_leavesdug"}
+        dug = {name = "blk_leavesdug"},
+        place_failed = {name = "blk_failed"}
     }
 end
