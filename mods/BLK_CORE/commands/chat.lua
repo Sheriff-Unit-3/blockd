0afb60cd78167280d453a45b977d22e7d3b561b1
Sheriff U3 <210896603+Sheriff-Unit-3@users.noreply.github.com>
2025-12-31 14:53:17 -0600

    Added wip command

--- mods/BLK_CORE/commands/chat.lua
+++ mods/BLK_CORE/commands/chat.lua
@@ -5,3 +5,14 @@ core.register_chatcommand("v", {
         return true, string
     end
 })
+--[[
+core.register_chatcommand("h", {
+    description = "Help message.",
+    func = function(name)
+        return true, core.show_formspec(name, blk.mod()..":help", "formspec_version[6]"..
+            "size[11,11]".."label[4.25,0.3;Blockd Help Menu]"..
+            "textlist[0.5,1;10,9.5;Commands;/v - Running Blockd version.,/h - Shows you this menu.;1;false]"..
+            "label[0.5,0.7;Commands]")
+    end
+})
+]]--
